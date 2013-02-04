c7808e6b7b070d578b36b31c8a19e65f338d603f
Mark Johnson <luther07@gmail.com>
2013-02-04 07:11:53 -0600

    Two SQL statements for inserting the suggested data into events table.

---
+++ 2-postgresql/insert_events.sql
@@ -0,0 +1,5 @@
+INSERT INTO events (title, starts, ends, event_id, venue_id)
+VALUES ('LARP Club', '2012-02-15 07:30:00', '2012-02-15 09:30:00', 1, 2);
+INSERT INTO events (title, starts, ends, event_id)
+VALUES ('April Fools Day', '2012-04-01 00:00:00', '2012-04-01 23:59:00', 2),
+('Christmas Day', '2012-12-25 00:00:00', '2012-12-25 23:59:00', 3);
