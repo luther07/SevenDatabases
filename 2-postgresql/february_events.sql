4a692d16336796bc75b74e24c16b1acd302e28f7
Mark Johnson <luther07@gmail.com>
2013-02-05 07:22:36 -0600

    DML for inserting some February data into the events table.

---
+++ 2-postgresql/february_events.sql
@@ -0,0 +1,8 @@
+INSERT INTO events (title,starts,ends,venue_id)
+VALUES ( 'Wedding', '2012-02-26 21:00:00','2012-02-26 23:00:00',
+       (SELECT venue_id FROM venues WHERE name = 'Voodoo Donuts') ),
+       ('Dinner with Mom','2012-02-26 18:00:00','2012-02-26 18:00:00',
+       (SELECT venue_id FROM venues WHERE name = 'My Place') );
+
+INSERT INTO events (title,starts,ends)
+VALUES ('Valentine''s Day','2012-02-14 00:00:00','2012-02-14 23:59:00');
