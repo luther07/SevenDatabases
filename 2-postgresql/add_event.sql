c1b0c754b59bbb03bc0096f17fbe19364cdae616
Mark Johnson <luther07@gmail.com>
2013-02-05 20:23:05 -0600

    Modified the names of the parameters to the FUNCTION add_event, and all appearances of of the parameters, adding 'i_' suffix.
    This fixes syntax errors which prevented this code from running
    in postgres 8.4.13.

--- 2-postgresql/add_event.sql
+++ 2-postgresql/add_event.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE FUNCTION add_event( title text, starts timestamp,
-  ends timestamp, venue text, postal varchar(9), country char(2) ) 
+CREATE OR REPLACE FUNCTION add_event( i_title text, i_starts timestamp,
+  i_ends timestamp, i_venue text, i_postal varchar(9), i_country char(2) ) 
 RETURNS boolean AS $$
 DECLARE
   did_insert boolean := false;
@@ -8,12 +8,12 @@ DECLARE
 BEGIN
   SELECT venue_id INTO the_venue_id
   FROM venues v
-  WHERE v.postal_code=postal AND v.country_code=country AND v.name ILIKE venue
+  WHERE v.postal_code=i_postal AND v.country_code=i_country AND v.name ILIKE i_venue
   LIMIT 1;
   
   IF the_venue_id IS NULL THEN
     INSERT INTO venues (name, postal_code, country_code)
-    VALUES (venue, postal, country)
+    VALUES (i_venue, i_postal, i_country)
     RETURNING venue_id INTO the_venue_id;
     
     did_insert := true;
@@ -23,7 +23,7 @@ BEGIN
   RAISE NOTICE 'Venue found %', the_venue_id;
 
   INSERT INTO events (title, starts, ends, venue_id)
-  VALUES (title, starts, ends, the_venue_id);
+  VALUES (i_title, i_starts, i_ends, the_venue_id);
 
   RETURN did_insert;
 END;
